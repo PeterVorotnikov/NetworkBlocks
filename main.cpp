2fa24a6c1ee0c407a7b15c7206527f6af35bf44d
p <p>
2024-05-02 17:01:13 +0300

    execution time comparison

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <time.h>
 #include "layers.h"
 
 int main() {
-	LinearLayer layer(2, 2, 2, 4);
-	vector<vector<double>> in = { {-1, 1}, {1, -1} };
+	long long start, end;
+	int b = 1000;
+	int d = 800;
+	LinearLayer layer(d, d, b, 8);
+	vector<vector<double>> in;
+	in.resize(b);
+	for (int i = 0; i < b; i++) {
+		in[i].resize(d);
+	}
+	start = clock();
 	layer.forward(in);
+	end = clock();
+	double t = (double)(end - start) / (double)(CLOCKS_PER_SEC);
+	cout << "Time: " << t << "s\n";
 	return 0;
 }
