1e2d1736e77e47f27a9e22d1a6535352c1412785
p <p>
2024-05-04 15:19:39 +0300

    add l2 to optimizers

--- main.cpp
+++ main.cpp
@@ -4,10 +4,38 @@
 #include "optimizers.h"
 
 int main() {
-	ConvolutionalLayer layer(10, 10, 2, 3, 1);
-	vector<vector<vector<vector<double>>>> in(1, vector<vector<vector<double>>>(2,
-		vector < vector < double>>(10, vector<double>(10, 1))));
-	layer.forward(in);
+	LinearLayer layer1(3, 3, 3);
+	ReLU1d activation(3, 3, 0.1);
+	LinearLayer layer2(3, 2, 3);
+	Adam1d biases1(3, 0.0003, 0.9, 0.99);
+	Adam2d weights1(3, 3, 0.0003, 0.9, 0.99);
+	Adam1d biases2(2, 0.0003, 0.9, 0.99);
+	Adam2d weights2(3, 2, 0.0003, 0.9, 0.99);
+	MSELoss loss(2, 3);
+	vector<vector<double>> in = { {-0.5, 0.6, 0.1}, {0.8, 0.8, 0}, {0.1, 0.2, 0.6} };
+	vector<vector<double>> out = { {0.7, 0.2}, {-0.7, 0.3}, {2, 1} };
+	for (int e = 0; e < 10000; e++) {
+		layer1.forward(in);
+		activation.forward(layer1.output);
+		layer2.forward(activation.output);
+		loss.calculate(layer2.output, out);
+		for (int b = 0; b < in.size(); b++) {
+			for (int i = 0; i < out[b].size(); i++) {
+				cout << layer2.output[b][i] << " ";
+			}
+			cout << endl;
+		}
+		cout << loss.value << "\n\n";
+		layer2.backward(activation.output, loss.diff);
+		activation.backward(layer1.output, layer2.diff);
+		layer1.backward(in, activation.diff);
+		biases1.step(layer1.biases, layer1.biasesDiff);
+		weights1.step(layer1.weights, layer1.weightsDiff);
+		biases2.step(layer2.biases, layer2.biasesDiff);
+		weights2.step(layer2.weights, layer2.weightsDiff);
+		layer1.zeroGradients();
+		layer2.zeroGradients();
+	}
 
 	return 0;
 }
--- optimizers.h
+++ optimizers.h
@@ -7,23 +7,24 @@ using namespace std;
 class Adam1d {
 public:
 	int d = 0;
-	double alpha = 0, beta1 = 0, beta2 = 0;
+	double alpha = 0, beta1 = 0, beta2 = 0, l2 = 0;
 	double epsilon = pow(10, -8);
 	vector<double> v;
 	vector<double> g;
 
 public:
-	Adam1d(int d, double alpha = 0.0003, double beta1 = 0.9, double beta2 = 0.99) {
+	Adam1d(int d, double alpha = 0.0003, double beta1 = 0.9, double beta2 = 0.99, double l2 = 0) {
 		this->d = d;
 		this->alpha = alpha;
 		this->beta1 = beta1;
 		this->beta2 = beta2;
+		this->l2 = l2;
 		v.resize(d);
 		g.resize(d);
 	}
 	void step(vector<double>& parameters, vector<double>& diff) {
 		for (int i = 0; i < d; i++) {
-			v[i] = beta1 * v[i] + (1 - beta1) * diff[i];
+			v[i] = beta1 * v[i] + (1 - beta1) * (diff[i] + 2 * l2 * parameters[i]);
 			g[i] = beta2 * g[i] + (1 - beta2) * pow(diff[i], 2);
 			parameters[i] -= alpha / sqrt(g[i] + epsilon) * v[i];
 		}
@@ -34,18 +35,20 @@ public:
 class Adam2d {
 public:
 	int d1 = 0, d2 = 0;
-	double alpha = 0, beta1 = 0, beta2 = 0;
+	double alpha = 0, beta1 = 0, beta2 = 0, l2 = 0;
 	double epsilon = pow(10, -8);
 	vector<vector<double>> v;
 	vector<vector<double>> g;
 
 public:
-	Adam2d(int d1, int d2, double alpha = 0.0003, double beta1 = 0.9, double beta2 = 0.99) {
+	Adam2d(int d1, int d2, double alpha = 0.0003, double beta1 = 0.9, double beta2 = 0.99, 
+		double l2 = 0) {
 		this->d1 = d1;
 		this->d2 = d2;
 		this->alpha = alpha;
 		this->beta1 = beta1;
 		this->beta2 = beta2;
+		this->l2 = l2;
 		v.resize(d1);
 		g.resize(d1);
 		for (int i = 0; i < d1; i++) {
@@ -56,7 +59,8 @@ public:
 	void step(vector<vector<double>>& parameters, vector<vector<double>>& diff) {
 		for (int i1 = 0; i1 < d1; i1++) {
 			for (int i2 = 0; i2 < d2; i2++) {
-				v[i1][i2] = beta1 * v[i1][i2] + (1 - beta1) * diff[i1][i2];
+				v[i1][i2] = beta1 * v[i1][i2] + (1 - beta1) * (diff[i1][i2] + 
+					2 * l2 * parameters[i1][i2]);
 				g[i1][i2] = beta2 * g[i1][i2] + (1 - beta2) * pow(diff[i1][i2], 2);
 				parameters[i1][i2] -= alpha / sqrt(g[i1][i2] + epsilon) * v[i1][i2];
 			}
